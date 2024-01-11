fe82e1a3fa603c7ff8076795f022b18fe37f0308
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-01-11 11:09:26 +0100

    Update printAllPairs.js

--- code/printAllPairs.js
+++ code/printAllPairs.js
@@ -1,3 +1,5 @@
+// Function to print all pairs of numbers up to n (exclusive) using nested loops.
+// Time complexity: O(n^2) - quadratic complexity due to nested loops.
 function printAllPairs(n) {
   for (var i = 0; i < n; i++) {
     for (var j = 0; j < n; j++) {
@@ -6,5 +8,8 @@ function printAllPairs(n) {
   }
 }
 
+// Retrieve the input value from the command line arguments.
 const arg = process.argv[2];
+
+// Call the printAllPairs function with the provided input value.
 printAllPairs(arg);
