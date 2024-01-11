ce014e241d9a98a4e9025e0f5d27e60904c66d79
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-01-11 12:16:57 +0100

    Update stacks-as-linked-list.js

--- data-structures/stacks-as-linked-list.js
+++ data-structures/stacks-as-linked-list.js
@@ -1,4 +1,4 @@
-// Stacks data structure- linked list implementation
+// Stacks Data Structure - Linked List Implementation
 class Node {
   constructor(value) {
     this.value = value;
@@ -8,51 +8,49 @@ class Node {
 
 class Stack {
   constructor() {
-    this.first = null;  // first instead of head
-    this.last = null;   // last instead of tail
+    this.first = null;  // 'first' instead of 'head'
+    this.last = null;   // 'last' instead of 'tail'
     this.size = 0;
   }
 
-  // just shift code from singly linked list
-  // remove from top of stack
+  // Remove from the top of the stack
   pop() {
-    if (!this.first) return null;  // if no first, then can't shift.
+    if (!this.first) return null;  // If no 'first', then can't pop.
 
     let removedTop = this.first;
-    // removedTop.next = null;  garbage collecting in js cleans up, b/c old first will not be used.
-    if (this.first === this.last) {   // only 1 element left?
-      this.last = null;               // set last to null to avoid it being populated w/ last item
+    if (this.first === this.last) {   // Only 1 element left?
+      this.last = null;               // Set 'last' to null to avoid it being populated with the last item
     }
-    this.first = this.first.next;     // set to null or next element
+    this.first = this.first.next;     // Set to null or the next element
     this.size--;
     return removedTop;
   }
 
-  // just unshift code from singly linked list
-  // add to top of stack- O(1)
+  // Add to the top of the stack - O(1)
   push(data) {
-    if (data === undefined) return undefined; // if not handled, will increment when nothing passed
+    if (data === undefined) return undefined; // If not handled, will increment when nothing is passed
 
     let newNode = new Node(data);
-    if (!this.first) {             // if no first aka length equals 0
+    if (!this.first) {             // If no 'first' aka length equals 0
       this.first = newNode;
       this.last = newNode;
     } else {
-      newNode.next = this.first;     // set new node pointer to first
-      this.first = newNode;          // set new node as first
+      newNode.next = this.first;     // Set the new node's pointer to 'first'
+      this.first = newNode;          // Set the new node as 'first'
     }
-    this.size++;                  // +1 since new item
-    return this.size;             // return size
+    this.size++;                  // Increment by 1 since it's a new item
+    return this.size;             // Return the size of the stack
   }
 
+  // Peek at the top without removing
   topPeek() {
     return this.first;
   }
 
+  // Check if the stack is empty
   isEmpty() {
     return this.size === 0 ? true : false;
   }
-  
 }
 
 let newStack = new Stack();
@@ -78,26 +76,25 @@ newStack.push(3);
 console.log(newStack.topPeek());
 
 /*
-Stack ADT (abstract data type)
-A list/collection with the restriction that insertion 
+Stack Abstract Data Type (ADT):
+A collection with the restriction that insertion 
 and deletion can only be performed from 
 one end, the top. 
 
-A list/collection with LIFO principle, 
+A collection with LIFO principle, 
 last in, first out.
 
 The last element is the first one out.
 
-Think of a stack of books or the
-Tower of Hanoi.
+Think of a stack of books or the Tower of Hanoi.
 
 Operations:
 All methods are O(1)
-push() - add to top of stack
-pop() - remove and return top of stack
-Top() - peek the top, don't remove
-isEmpty() - true/false
-length() - amount of elements
+push() - Add to the top of the stack
+pop() - Remove and return the top of the stack
+top() - Peek the top without removing
+isEmpty() - True/False based on whether the stack is empty
+length() - Return the number of elements in the stack
 
 The two main things that you have to care about
 in a stack are push() and pop().
@@ -109,37 +106,37 @@ Access- accessing element
 Uses:
 Function calls/Recursion
 Browser history
-undo operations in text editors
-used in algorithms: balanes parentheses for compilers
+Undo operations in text editors
+used in algorithms: balance parentheses for compilers
 call stack
 
 Implementation:
-JavaScript doesn't come with an implemention 
+JavaScript doesn't come with an implementation 
 of a stack.
 
 You can implement it with an array or linked list.
-An linked list allows you to increase size of stack 
+A linked list allows you to increase the size of the stack 
 beyond the fixed memory allocation of an array.
 
-Adding to beginning of array in stack implementation 
-is O(n), since we have to re-index all of the elements.
-Best to add to the end, since its O(1).
+Adding to the beginning of an array in a stack implementation 
+is O(n), since you have to re-index all of the elements.
+Best to add to the end, since it's O(1).
 
-If you care about efficency, all you really need is 
+If you care about efficiency, all you really need is 
 LIFO property. We won't care about accessing info 
-based off of index, since that would mean taking things
-from middle of the stack. So, its better to use a linked list 
+based on index, since that would mean taking things
+from the middle of the stack. So, it's better to use a linked list 
 since it can grow dynamically.
 
-If using linked list, insert at head.
-Removing at tail will be O(n) b/c
-you have to traverse to second to last node.
+If using linked list, insert at the head.
+Removing at tail will be O(n) because
+you have to traverse to the second-to-last node.
 Why? You remove the last node, then you 
 have to update the tail property with the 
 new last node.
 
 Better to add/remove from the head, since 
-its one direction, O(1).
+it's one direction, O(1).
 
 You could use a doubly linked list, but 
 that would take up more memory.
