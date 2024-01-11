98dbc88610da9e5bd52bab9c5d79c1a6eb3fbd35
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2024-01-11 11:05:10 +0100

    Update addUpTo.js

--- code/addUpTo.js
+++ code/addUpTo.js
@@ -1,18 +1,24 @@
-// naive solution
+// This code includes two functions to calculate the sum of integers from 1 to n,
+// and measures the time taken by each function for a large input value.
+
+// Function to calculate the sum of integers from 1 to n using a naive approach.
 function addUpToNaive(n) {
-  let total = 0;  // accumulator
+  let total = 0;
   for (let i = 1; i <= n; i++) {
-    total += i;  // loop over
+    total += i; 
   }
   return total;
 }
 
-// more optimized solution
+// Function to optimize the calculation of the sum of integers from 1 to n.
 function addUpToOptimized(n) {
   return n * (n + 1) / 2;
 }
 
+// Retrieve the input value from the command line arguments.
 const arg = process.argv[2];
+
+// Calculate and display the results for both the naive and optimized functions.
 const result1 = addUpToNaive(arg);
 const result2 = addUpToOptimized(arg);
 
@@ -20,16 +26,17 @@ console.log(`input: ${arg}`);
 console.log(`result (Naive): ${result1}`);
 console.log(`result (Optimized): ${result2}`);
 
-// naive way to evaluate speed using timers
-// not reliable way to objectively measure algos
-// since computers work at different speeds
+// Define a constant for one billion, used for converting time measurements.
 const ONE_BILLION = 1000000000;
+
+// Measure the time taken by the naive function for a large input value.
 let t1 = process.hrtime.bigint();
 addUpToNaive(100000000);
 let t2 = process.hrtime.bigint();
-console.log(`Naive Time elapse: ${parseInt(t2 - t1) / ONE_BILLION } seconds`);
+console.log(`Naive Time elapsed: ${parseInt(t2 - t1) / ONE_BILLION } seconds`);
 
+// Measure the time taken by the optimized function for a large input value.
 let t3 = process.hrtime.bigint();
 addUpToOptimized(100000000);
 let t4 = process.hrtime.bigint();
-console.log(`Optimized Time elapse: ${parseInt(t4 - t3) / ONE_BILLION }seconds`);
+console.log(`Optimized Time elapsed: ${parseInt(t4 - t3) / ONE_BILLION } seconds`);
